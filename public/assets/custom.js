4a89c533c9d8d8786b15be03379d14d4e11dc70f
Stella Lie <ask@stellalie.com>
2017-01-06 00:22:41 +1100

    More

--- public/assets/custom.js
+++ public/assets/custom.js
@@ -1,4 +1,19 @@
 jQuery(document).ready(function($) {
+    // Smooth scrolling
+    $('a[href*="#"]:not([href="#"])').click(function() {
+        if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+            var target = $(this.hash);
+            target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+            if (target.length) {
+                $('html, body').animate({
+                    scrollTop: target.offset().top
+                }, 1000);
+                return false;
+            }
+        }
+    });
+
+    // Carousel initialisation
     $('.carousel').each(function () {
         // Stop the carousel from auto move
         $(this).carousel({
